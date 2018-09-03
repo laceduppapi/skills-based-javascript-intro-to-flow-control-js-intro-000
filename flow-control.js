5ec6494714482e32037ca596b5a8961c27cc3678
lacedup236@gmail.com <lacedup236@gmail.com>
2018-09-03 18:14:48 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -13,6 +13,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+<<<<<<< HEAD
 if (age >= 13 && age <= 19) {
   return "You are a teenager!"
 } else if (age <=12) {
@@ -20,6 +21,18 @@ if (age >= 13 && age <= 19) {
 } else {
   return "You are a grownup"
 }
+=======
+  switch (age) {
+    case age >= 13 && age <= 19:
+      return "You are a teenager!"
+      break;
+      case age >= 12:
+        return "You are a kid"
+        break;
+
+    default: 'idk'
+  }
+>>>>>>> 18a494c1706eeae83ce5730d465db2e3c951de1c
   }
 
 
@@ -35,6 +48,7 @@ switch (age) {
      case 14:
      return "You are a teenager"
      break;
+<<<<<<< HEAD
       case 15:
       return "You are a teenager"
       break;
@@ -48,6 +62,21 @@ switch (age) {
              return "You are a teenager"
              break;
                case 19:
+=======
+     case 15:
+       return "You are a teenager"
+       break;
+       case 16:
+         return "You are a teenager"
+         break;
+         case 17:
+           return "You are a teenager"
+           break;
+           case 18:
+             return "You are a teenager"
+             break;
+             case 19:
+>>>>>>> 18a494c1706eeae83ce5730d465db2e3c951de1c
                return "You are a teenager"
                break;
   default:
